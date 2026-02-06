eec6d3cbc005154ed580dc6b40367e9c457947d0
marcfernandezo <marc.fernaandezo@gmail.com>
2026-02-06 10:07:36 +0100

    add: create character warrior class

---
+++ src/CharacterWarrior.java
@@ -0,0 +1,18 @@
+public class CharacterWarrior extends Entity {
+    public CharacterWarrior() {
+        super.setName("Zed");
+        super.setLevel(5);
+        super.setHeartPoints(350);
+        super.setMana(150);
+
+        super.setAttackPoints(38);
+        super.setMagicAttackPoints(5);
+    }
+
+    public void attack(Entity character) {
+        int hitPoints = this.getAttackPoints() + this.getMagicAttackPoints();
+        this.setMana(this.getMana() - 20);
+
+        character.setHeartPoints(character.getHeartPoints() - hitPoints);
+    }
+}
