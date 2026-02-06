d94acb5519262cce42d8da17184e500b5274caad
marcfernandezo <marc.fernaandezo@gmail.com>
2026-02-06 12:53:36 +0100

    create: characters array list initialize

--- src/Characters.java
+++ src/Characters.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Characters {
+
+    public static void init(List<Entity> charactersList) {
+        charactersList.add(new CharacterWizard());
+        charactersList.add(new CharacterWarrior());
+        charactersList.add(new CharacterArcher());
+    }
 }
