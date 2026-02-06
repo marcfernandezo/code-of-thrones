93aff9487c20cf44ca4f84efcceed017a57a9cfd
marcfernandezo <marc.fernaandezo@gmail.com>
2026-02-06 12:38:05 +0100

    add: create archer character class

---
+++ src/CharacterArcher.java
@@ -0,0 +1,19 @@
+public class CharacterArcher extends Entity {
+    public CharacterArcher() {
+        super.setName("Gem");
+        super.setLevel(4);
+        super.setHeartPoints(250);
+        super.setMana(80);
+
+        super.setAttackPoints(30);
+        super.setMagicAttackPoints(9);
+    }
+
+    @Override
+    public void attack(Entity character) {
+        int hitPoints = this.getAttackPoints() + this.getMagicAttackPoints();
+        this.setMana(this.getMana() - 20);
+
+        character.setHeartPoints(character.getHeartPoints() - hitPoints);
+    }
+}
